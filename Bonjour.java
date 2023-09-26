3f0e595e55c31da078f596e0f1d1fadeb12fd68f
RomainHauet <84860538+RomainHauet@users.noreply.github.com>
2023-09-26 17:46:12 +0200

    Create Bonjour.java
    Ajoutre un fichier Bonjour.java qui dit bonjour dans la console

---
+++ Bonjour.java
@@ -0,0 +1,9 @@
+//Programme qui dit bonjour
+
+public class Bonjour {
+
+    public static void main(String[] args) {
+        System.out.println("Bonjour, je m'appelle William Prévost");
+    }
+
+}
